8631523307fef6fdec9f81dd915d8b1e094adbcc
carrascomj <carrascomurielj@gmail.com>
2022-06-17 18:22:36 +0200

    fix: update priors to new version

--- update_maud/update_priors.py
+++ update_maud/update_priors.py
@@ -1,12 +1,29 @@
 """Update priors file to newer maud impl."""
 
+import re
+from copy import deepcopy
+
 import click
 import pandas as pd
 import toml
 
 from .update_model_toml import UNDER_PAT, ModelNew
 
-NEW_PRIOR_COLS = ["parameter", "row_id", "col_id", "location", "scale", "pct1", "pct99"]
+NEW_PRIOR_COLS = [
+    "parameter",
+    "metabolite",
+    "compartment",
+    "enzyme",
+    "reaction",
+    "experiment",
+    "location",
+    "scale",
+    "pct1",
+    "pct99",
+]
+# the comparment is represented by a single letter,
+# prefixed by underscored at the end of the entity. 
+COMP_PAT = re.compile(r"(.*)_([a-z]$)")
 
 
 def query_lookup(
@@ -14,6 +31,7 @@ def query_lookup(
     model: ModelNew,
     query_ns: str = "enzyme_id",
     lookup: str = "enzyme_reaction",
+    key: str = "reaction_id",
 ):
     """Lookup the corresponding reaction of an enzyme.
 
@@ -21,7 +39,7 @@ def query_lookup(
     """
     enz_id = query_id.replace("_", "")
     return next(
-        enz_reac.id
+        enz_reac.__getattribute__(key)
         for enz_reac in model.__getattribute__(lookup)
         if enz_reac.__getattribute__(query_ns) == enz_id
     )
@@ -40,71 +58,48 @@ def is_valid_enzyme(enzyme_id: str, model: ModelNew):
 
 def update_priors(priors_df: pd.DataFrame, model: ModelNew) -> pd.DataFrame:
     df = priors_df.copy()
-    df["col_id"] = None
-    df = df.rename({"parameter_type": "parameter"}, axis=1)
-    df.enzyme_id = df.enzyme_id.str.replace("_", "")
-    df.mic_id = df.mic_id.apply(
-        lambda x: UNDER_PAT.sub("", x) if isinstance(x, str) else x
+    target_cols = deepcopy(NEW_PRIOR_COLS)
+    df = df.rename(
+        {
+            "parameter_type": "parameter",
+            "enzyme_id": "enzyme",
+            "experiment_id": "experiment",
+            "mic_id": "metabolite",
+            # will fill out the other reactions later
+            "drain_id": "reaction",
+        },
+        axis=1,
     )
-    df.experiment_id = df.experiment_id.str.replace("_", "")
+    df["compartment"] = None
+    # comparment separation of metabolites
+    df.loc[~pd.isna(df.metabolite), "compartment"] = df.loc[
+        ~pd.isna(df.metabolite), "metabolite"
+    ].apply(lambda x: COMP_PAT.sub(r"\2", x))
+    df.loc[~pd.isna(df.metabolite), "metabolite"] = df.loc[
+        ~pd.isna(df.metabolite), "metabolite"
+    ].apply(lambda x: COMP_PAT.sub(r"\1", x))
+    # underscores are forbidden
+    df.enzyme = df.enzyme.str.replace("_", "")
+    df.experiment = df.experiment.str.replace("_", "")
+    df.metabolite = df.metabolite.str.replace("_", "")
+    df.parameter = df.parameter.str.replace("diss_t", "dissociation_constant")
+    df.parameter = df.parameter.str.replace("conc_phos", "conc_pme")
     # make sure that every enzyme is actually in the final config
     df = df.loc[
         ~df.parameter.isin(["kcat", "conc_enzyme"])
-        | df.enzyme_id.apply(lambda x: is_valid_enzyme(x, model)),
+        | df.enzyme.apply(lambda x: is_valid_enzyme(x, model)),
         :,
     ]
-    # kcats
-    df.loc[df.parameter == "kcat", "row_id"] = df.loc[
-        df.parameter == "kcat", "enzyme_id"
+    # add reaction ids
+    df.loc[df.parameter.isin(["kcat", "ki"]), "reaction"] = df.loc[
+        df.parameter.isin(["kcat", "ki"]), "enzyme"
     ].apply(lambda x: query_lookup(x, model))
-    # kms
-    kms = df[df.parameter == "km"]
-    df.loc[df.parameter == "km", "row_id"] = kms.enzyme_id + "_" + kms.mic_id
-    # ki
-    df.loc[df.parameter == "ki", "row_id"] = (
-        df.loc[df.parameter == "ki", "enzyme_id"].apply(
-            lambda x: query_lookup(x, model)
-        )
-        + "_"
-        + df.loc[df.parameter == "ki", "mic_id"]
-    )
-    # transfer and dissociation constants; phosphorilation
-    dissociation_constants = df[df.parameter == "diss_t"]
-    df.loc[df.parameter == "diss_t", "row_id"] = (
-        dissociation_constants.enzyme_id + "_" + dissociation_constants.mic_id
-    )
-    df.parameter = df.parameter.str.replace("diss_t", "dissociation_constant")
-    df.loc[df.parameter.isin(["transfer_constant", "kcat_phos"]), "row_id"] = df.loc[
-        df.parameter.isin(["transfer_constant", "kcat_phos"]), "enzyme_id"
-    ]
-    conc_phos = df[df.parameter == "conc_phos"]
-    df.loc[df.parameter == "conc_phos", "row_id"] = (
-        conc_phos.experiment_id + "_" + conc_phos.enzyme_id
-    )
-    # psi
-    df.loc[df.parameter == "psi", "row_id"] = df.loc[
-        df.parameter == "psi", "experiment_id"
-    ]
-    # conc_unbalanced
-    df.loc[df.parameter == "conc_unbalanced", "row_id"] = df.loc[
-        df.parameter == "conc_unbalanced", "experiment_id"
-    ]
-    df.loc[df.parameter == "conc_unbalanced", "col_id"] = df.loc[
-        df.parameter == "conc_unbalanced", "mic_id"
-    ]
-    # protein concentrations
-    df.loc[df.parameter == "conc_enzyme", "row_id"] = df.loc[
-        df.parameter == "conc_enzyme", "experiment_id"
-    ]
-    df.loc[df.parameter == "conc_enzyme", "col_id"] = df.loc[
-        df.parameter == "conc_enzyme", "enzyme_id"
-    ]
-    # drains
-    drains = df[df.parameter == "drain"]
-    df.loc[df.parameter == "drain", "row_id"] = (
-        drains.experiment_id + "_" + drains.drain_id
-    )
-    df = df[NEW_PRIOR_COLS]
+    if (df.parameter == "conc_phos").any():
+        target_cols.append("phosphorylation_modifying_enzyme")
+        # TODO(jorge): I have to look up an example of this
+        raise NotImplementedError("Phosphorylation update is not implemented")
+    # the previous steps are only succesful if this works
+    df = df[target_cols]
     return df
 
 
